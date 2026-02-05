3dc86fff64c5d46b7c4c15134eb50398367c750a
Alex-Kurylovich <102453667+Alex-Kurylovich@users.noreply.github.com>
2026-02-04 19:07:13 -0500

    fix echo comment

--- gitscript.sh
+++ gitscript.sh
@@ -19,7 +19,7 @@ case "$1" in
           echo "zip it $2"
           zip -q -r $2 $2 -x "$2/target/*" "$2/**/target/*" "$2/.git/*" "$2/.idea/*" "$2/.classpath/*" "$2/.project/*" "$2/**/node_modules/*"
       else
-          echo "The second argument zip git repo does not exist or is empty."
+          echo "The second argument does not exist or is empty."
           for entry in "$search_dir"/*; do
             if [ -d $entry ]; then
               echo "zip it $entry"
