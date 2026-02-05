5d5835d290c26d28774f3655bb0122ce2c4e22a9
Alex-Kurylovich <102453667+Alex-Kurylovich@users.noreply.github.com>
2026-02-04 19:03:29 -0500

    add $2 argument

--- gitscript.sh
+++ gitscript.sh
@@ -14,12 +14,19 @@ case "$1" in
   "gitrepozip")
       echo 'Read the git directory and zip it.'
       search_dir=.
-      for entry in "$search_dir"/*; do
-        if [ -d $entry ]; then
-          echo "zip it $entry"
-          zip -q -r $entry $entry -x "$entry/target/*" "$entry/**/target/*" "$entry/.git/*" "$entry/.idea/*" "$entry/.classpath/*" "$entry/.project/*" "$entry/**/node_modules/*"
-        fi
-      done
+      if [ -n "$2" ]; then
+          echo "The second argument exists and is not empty."
+          echo "zip it $2"
+          zip -q -r $2 $2 -x "$2/target/*" "$2/**/target/*" "$2/.git/*" "$2/.idea/*" "$2/.classpath/*" "$2/.project/*" "$2/**/node_modules/*"
+      else
+          echo "The second argument zip git repo does not exist or is empty."
+          for entry in "$search_dir"/*; do
+            if [ -d $entry ]; then
+              echo "zip it $entry"
+              zip -q -r $entry $entry -x "$entry/target/*" "$entry/**/target/*" "$entry/.git/*" "$entry/.idea/*" "$entry/.classpath/*" "$entry/.project/*" "$entry/**/node_modules/*"
+            fi
+          done
+      fi
       exit
       ;;
   *)
