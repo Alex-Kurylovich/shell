142b1279e7a89c6a27aaedb244b132d812df4855
Alex-Kurylovich <102453667+Alex-Kurylovich@users.noreply.github.com>
2026-03-02 03:22:49 -0500

    work in the parent directory

--- gitscript.sh
+++ gitscript.sh
@@ -2,8 +2,8 @@
 
 echo 'This is git directory script assuming that parent is git repositories.'
 
-rm -f ../gitrepos
-cp -f ./gitscript.sh ../gitscript.sh
+#rm -f ../gitrepos
+#cp -f ./gitscript.sh ../gitscript.sh
 
 case "$1" in
   "")
@@ -13,23 +13,24 @@ case "$1" in
   "gitrepourl")
       echo 'Read git remote entries in your repositories.'
       echo 'Read ./git/config url from all projects and create git urls file gitrepos'
-      cat ./*/.git/config | grep "url = " | sort | awk '{print substr($0, 8)}' > gitrepos
+      cat ../*/.git/config | grep "url = " | sort | awk '{print substr($0, 8)}' > ./gitrepos
       echo 'Print gitrepos'
       cat gitrepos
       exit
       ;;
   "gitrepozip")
       echo 'Read the git directory and zip it.'
+      REPO=../$2
       if [ -n "$2" ]; then
           echo "The second argument exists and is not empty."
-          echo "zip it $2"
-          zip -q -r $2 $2 -x "$2/target/*" "$2/**/target/*" "$2/.git/*" "$2/.idea/*" "$2/.classpath/*" "$2/.project/*" "$2/**/node_modules/*"
+          echo "zip it $REPO"
+          zip -q -r $REPO $REPO -x "$REPO/target/*" "$REPO/**/target/*" "$REPO/.git/*" "$REPO/.idea/*" "$REPO/**/.idea/*" "$REPO/.classpath/*" "$REPO/.project/*"  "$REPO/node_modules/*" "$REPO/**/node_modules/*"
       else
           echo "The second argument does not exist or is empty, zip all."
-          for entry in ./*; do
+          for entry in ../*; do
             if [ -d $entry ]; then
               echo "zip it $entry"
-              zip -q -r $entry $entry -x "$entry/target/*" "$entry/**/target/*" "$entry/.git/*" "$entry/.idea/*" "$entry/.classpath/*" "$entry/.project/*" "$entry/**/node_modules/*"
+              zip -q -r $entry $entry -x "$entry/target/*" "$entry/**/target/*" "$entry/.git/*" "$entry/.idea/*" "$entry/**/.idea/*" "$entry/.classpath/*" "$entry/.project/*" "$entry/**/node_modules/*"
             fi
           done
       fi
