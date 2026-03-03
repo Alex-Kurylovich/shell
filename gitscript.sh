d54b94f2bc88d597a5f700c03f5e94df17dbe28d
Alex-Kurylovich <102453667+Alex-Kurylovich@users.noreply.github.com>
2026-03-02 22:40:00 -0500

    completed and tested

--- gitscript.sh
+++ gitscript.sh
@@ -2,6 +2,31 @@
 
 echo 'This is git directory script assuming that parent is git repositories.'
 
+is_valid_url() {
+    local url=$1
+    # A robust regex to match a wide range of URLs
+    local url_regex='^(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]\.[-A-Za-z0-9\+&@/%?=~_|]*[-A-Za-z0-9\+&@#/%=~_|]$'
+    if [[ "$url" =~ $url_regex ]]; then
+        return 0 # Valid format
+    else
+        return 1 # Invalid format
+    fi
+}
+
+check_url_reachability() {
+    local url=$1
+    # Use curl with specific flags:
+    # -f, --fail: Fail silently (no output at all) on server errors (HTTP status >= 400)
+    # -s, --silent: Silent mode (disables progress meter and error messages)
+    # -o /dev/null: Redirect output to /dev/null
+    # --head: Fetch only HTTP headers (faster than downloading the whole page)
+    if curl --output /dev/null --silent --head --fail "$url"; then
+        return 0 # url is up/reachable
+    else
+        return 1 # url is down/unreachable or invalid
+    fi
+}
+
 case "$1" in
   "")
       echo 'Init is completed, change dir to parent to run gitscript.sh commands.'
@@ -27,7 +52,7 @@ case "$1" in
           for entry in ../*; do
             if [ -d $entry ]; then
               echo "zip it $entry"
-              zip -q -r $entry $entry -x "$entry/target/*" "$entry/**/target/*" "$entry/.git/*" "$entry/.idea/*" "$entry/**/.idea/*" "$entry/.classpath/*" "$entry/.project/*" "$entry/**/node_modules/*"
+              zip -q -r $entry $entry -x "$entry/target/*" "$entry/**/target/*" "$entry/.git/*" "$entry/.idea/*" "$entry/**/.idea/*" "$entry/.classpath/*" "$entry/.project/*" "$entry/node_modules/*" "$entry/**/node_modules/*"
             fi
           done
       fi
@@ -47,10 +72,20 @@ case "$1" in
       START_CHAR="/"
       END_CHAR="."
       for entry in "${my_array[@]}"; do
+          echo "$entry"
           temp="${entry##*$START_CHAR}"
           REPO="${temp%%$END_CHAR*}"
-          echo "git clone $REPO ../$REPO"
-#          git clone $entry
+          if is_valid_url "$entry"; then
+              echo "has a valid format."
+              if check_url_reachability "$entry"; then
+                  echo "server is up."
+                  git clone $entry ../$REPO
+              else
+                  echo "server is down or does not exist." >&2
+              fi
+          else
+              echo "does not have a valid format." >&2
+          fi
       done
       exit
       ;;
@@ -58,3 +93,5 @@ case "$1" in
       echo 'Wrong command' "$1"
       exit
 esac
+
+
